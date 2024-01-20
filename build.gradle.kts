ddd8f0ce9cf738ce5dc35c7bc3f648680b38ad55
Jacky-Bruse <6067707+Jacky-Bruse@users.noreply.github.com>
2024-01-20 12:10:46 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,7 +48,7 @@ subprojects {
 
             externalNativeBuild {
                 cmake {
-                    abiFilters("arm64-v8a", "armeabi-v7a", "x86", "x86_64")
+                    abiFilters("arm64-v8a", "x86_64")
                 }
             }
 
